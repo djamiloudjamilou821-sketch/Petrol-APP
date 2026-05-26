af477ac18e1a725b66dc4ea1eacb35d5f2b2a98a
djamiloudjamilou821-sketch <djamiloudjamilou821@gmail.com>
2026-05-26 18:18:45 +0000

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,75 @@
+const CACHE_NAME = "petroapp-v1";
+
+const urlsToCache = [
+
+    "/",
+    "/home",
+    "/lessons",
+    "/quiz",
+    "/formulas",
+    "/converter",
+
+    "/static/bg.jpg",
+    "/static/icon-192.png",
+    "/static/icon-512.png"
+
+];
+
+
+// INSTALL
 self.addEventListener("install", event => {
-    console.log("Service Worker Installed");
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache => {
+
+            return cache.addAll(urlsToCache);
+
+        })
+
+    );
+
 });
 
-self.addEventListener("activate", event => {
-    console.log("Service Worker Activated");
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(response => {
+
+            // RETURN CACHE FIRST
+            if (response) {
+                return response;
+            }
+
+            // OTHERWISE FETCH INTERNET
+            return fetch(event.request)
+
+            .then(networkResponse => {
+
+                return caches.open(CACHE_NAME)
+
+                .then(cache => {
+
+                    cache.put(
+                        event.request,
+                        networkResponse.clone()
+                    );
+
+                    return networkResponse;
+
+                });
+
+            });
+
+        })
+
+    );
+
 });
